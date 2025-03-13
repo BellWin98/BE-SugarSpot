29a907103718d9dc55b563332df4ae5800613870
BellWin98 <jongseunghan@naver.com>
2025-03-13 16:40:49 +0900

    initial commit

---
+++ src/main/java/com/sugarspot/SugarspotApplication.java
@@ -0,0 +1,13 @@
+package com.sugarspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarspotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugarspotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sugarspot/SugarspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugarspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugarspotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
